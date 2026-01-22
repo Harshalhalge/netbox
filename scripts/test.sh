55552988e85d7a2514b3c1b9e599ad5f9a2a9be9
harshalhalge <126957771+Harshalhalge@users.noreply.github.com>
2026-01-22 09:53:43 +0530

    Create test.sh

---
+++ scripts/test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+############################################
+# Server Inventory Script
+# Created from provided infrastructure data
+############################################
+
+# Default SSH Port
+DEFAULT_PORT=19022
+
+############################################
+# Application Server
+############################################
+APP_NAME="Application Server"
+APP_PRIVATE_IP="172.17.1.7"
+APP_PUBLIC_IP="180.94.16.170"
+APP_USER_ROOT="root"
+APP_PASS_ROOT="fSf)L$3PwMN&"
+APP_USER_SEC="usercnergeesec"
+APP_PASS_SEC="c}}cuf3phU{7ou5@9ogsj$.dG"
+
+############################################
+# API Servers
+############################################
+API1_PRIVATE_IP="172.17.1.5"
+API1_PUBLIC_IP="180.94.16.168"
+API1_USER_ROOT="root"
+API1_PASS_ROOT="pNB8OLOn6uR]"
+API1_USER_SEC="cnergeesecuser"
+API1_PASS_SEC="YK7C;uTE@XTQ%.>v"
+
+API2_PRIVATE_IP="172.17.1.6"
+API2_PUBLIC_IP="180.94.16.169"
+API2_USER_ROOT="root"
+API2_PASS_ROOT="Kw5df7cUtzCU"
+API2_USER_SEC="seccnergeeuser"
+API2_PASS_SEC="3F>,+<H7}=u;ZNvL~eu>(+}6Z"
+
+############################################
+# MongoDB Server
+##############################
